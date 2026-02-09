2f63bdfb82fee5188409e76c0f3f528716cce13e
Nicolas Pinier <nicolaspinier@MacBook-Air-de-Nicolas.local>
2026-02-09 23:05:45 +0100

    add money-pig img

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import { setTimer } from './components/timer/timer';
 import { setCigarretteCount, setMoneyCount, setTimeSavedCount } from './components/stats/moneyCount';
+import moneyPig from '../public/money-pig.png';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <header>
@@ -8,14 +9,15 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <div class="text-xl rounded p-1 px-2 border bg-neutral-700" id="time-timer"></div>
     </nav>
   </header>
-  <main class="p-4 justify-center flex">
-    <div class="flex flex-col items-baseline">
+  <main class="p-4 justify-centerflex">
+    <div class="flex flex-col items-center">
       <h2 id="money-count" class="gravitas-one-regular text-6xl transition-color duration-500"></h2>
       <div class="flex gap-4 justify-center opacity-75"> 
         <span><h3 id="cigarrette-count" class="flex items-center text-lg gap-2"></h3></span>
         |
         <span><h3 id="timesaved-count" class="flex items-center text-lg gap-2"></h3></span>
       </div>
+       <img src="${moneyPig}" alt="pink money pig with a gold coin overhead" width="200px" heigth="200px" />
     </div>
   </main>
 `;
