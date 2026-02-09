416ebfb2da4a3ae2af77afac24cb2f85523ed7ef
Nicolas Pinier <nicolaspinier@MacBook-Air-de-Nicolas.local>
2026-02-09 23:12:38 +0100

    add shadow to timer

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import moneyPig from '../public/money-pig.png';
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <header>
     <nav class="flex justify-end w-full">
-      <div class="text-xl rounded p-1 px-2 border bg-neutral-700" id="time-timer"></div>
+      <div class="text-xl rounded p-1 px-2 inset-shadow-sm inset-shadow-neutral-900 bg-neutral-700" id="time-timer"></div>
     </nav>
   </header>
   <main class="p-4 justify-centerflex">
